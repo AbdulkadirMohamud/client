fadd66f69afe0492190afe6a6599ee71c7a8251a
Abdulkadir <qansax30@hotmail.com>
2024-03-10 22:20:15 +0100

    socket.io and React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  // Kamer State
+  const [kamer, setKamer] = useState("");
+
+  // Berichten States
+  const [bericht, setBericht] = useState("");
+  const [berichtontvangen, setBerichtontvangen] = useState("");
+
+  const deelnemenkamer = () => {
+    if (kamer !== "") {
+      socket.emit("deelnemen_kamer", kamer);
+    }
+  };
+
+  const verstuurBericht = () => {
+    socket.emit("verstuur_bericht", { bericht, kamer });
+  };
+
+  useEffect(() => {
+    socket.on("bericht_ontvangen", (data) => {
+      setBerichtontvangen(data.bericht);
+    });
+  }, [socket]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Kamer Nummer..."
+        onChange={(event) => {
+          setKamer(event.target.value);
+        }}
+      />
+      <button onClick={deelnemenkamer}>deelnemen Kamer</button>
+      <input
+        placeholder="Bericht..."
+        onChange={(event) => {
+          setBericht(event.target.value);
+        }}
+      />
+      <button onClick={verstuurBericht}>berichtversturen</button>
+      <h1>bericht:</h1>
+      {berichtontvangen}
     </div>
   );
 }
